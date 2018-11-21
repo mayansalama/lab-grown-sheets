804ba7905e3ced8eb7ff514b823be7e05c692e6f
mayan.s <micsalama@gmail.com>
2018-11-21 12:29:38 +1100

    fixed setup

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 setup(name='lab-grown-sheets',
       version='0.1',
       description='Various helpful tools',
-      packages=['labgrownsheets'],
+      package_dir={'': 'labgrownsheets'},
       install_requires=['networkx>=1.11'],
       zip_safe=False)
